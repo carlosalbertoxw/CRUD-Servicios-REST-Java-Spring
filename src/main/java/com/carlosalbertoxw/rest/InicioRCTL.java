6bb9b032dbf7643afa0ed22d4c1befe68f408854
Carlos Alberto <carlosalbertoxw@gmail.com>
2018-06-25 19:46:18 -0500

    Se renombro controlador

---
+++ src/main/java/com/carlosalbertoxw/rest/InicioRCTL.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carlosalbertoxw.rest;
+
+import com.carlosalbertoxw.utilities.JSON;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Carlos
+ */
+@RestController
+public class InicioRCTL {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    private String inicio() {
+        return JSON.successToJson("Inicio");
+    }
+}
